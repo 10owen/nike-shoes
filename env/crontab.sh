125c910c302164ec51d07d11f01210bc99048e6b
Owen <zeros19861@gmail.com>
2021-10-30 14:19:24 +0900

    테스트 용이성을 위해 매분 스크립트 돌아가도록 변경, 쉘 선언 추가

--- env/crontab.sh
+++ env/crontab.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "10 * * * * python /home/nke/nike-shoes/module/parse.py >> /tmp/test.log" >> mycron
+echo "* * * * * python /home/nike/nike-shoes/module/parse.py >> /tmp/test.log" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
