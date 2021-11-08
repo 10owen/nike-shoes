13c338e0a0419abe8cc06424ce1a23d0b22d666f
10owen <zeros19861@gmail.com>
2021-11-09 00:34:34 +0900

    python requests 로 요청을 보내면 방어가 되는듯하여 curl 명령어로 바꿈, 리스폰스에 대한 해석 코드 추가

--- module/parse.py
+++ module/parse.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python
 
 import requests
+import os
+from datetime import datetime
+# os.system('curl -s "https://www.nike.com/kr/launch/?type=upcoming"  > /tmp')
+os.system('curl -s "https://www.nike.com/kr/launch/?type=upcoming" | grep 응모 > /tmp/nike_temp.log')
 
-url = "https://www.nike.com/kr/launch/?type=upcoming"
-header={"Accept-Encoding": "gzip", "Content-Type":"application/json"}
-response=requests.get(url, headers=header)
-print(response.text)
+f = open('/tmp/nike_temp.log', 'r')
+month = datetime.now().month
+day = datetime.now().day
+
+while True:
+    line = f.readline()
+    if not line: break
+    if "available-date-component" in line and "응모" in line:
+        print(line)
+        line = line.strip()
+        start = line.find(">")
+        end = line.rfind("<")
+        draw_info = line[start+1:end]
+        print(draw_info)
+
+        draw_info = draw_info.split()
+        for di in draw_info:
+            if "/" in di:
+                date_info = di.split("/")
+                if(int(date_info[0]) == month and int(date_info[1]) != day):
+                    print("bingo!!")
+        
+f.close()
