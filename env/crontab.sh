def9ba2b89dfb090a9a02d00e1a09a29c9fd8fb2
10owen <zeros19861@gmail.com>
2021-11-16 01:22:35 +0900

    utc 기준으로 크론 시간 설정. 드로우는 항상 10시에 시작되므로 10시 5분으로 맞춰놓음

--- env/crontab.sh
+++ env/crontab.sh
@@ -3,7 +3,7 @@
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "* * * * * python /home/nike/nike-shoes/module/parse.py >> /tmp/test.log" >> mycron
+echo "5 1 * * * python /home/nike/nike-shoes/module/parse.py >> /tmp/test.log" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
