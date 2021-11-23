add4b6a9de067ae97f1d67f77ec84fb1961c0f4d
10owen <zeros19861@gmail.com>
2021-11-24 00:31:00 +0900

    미래 드로우에 대한 정보가 있는 경우 멤캐시에 저장하는 기능 추가

--- module/parse.py
+++ module/parse.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
+#TODO : parser 로 모듈화하여 main 로직이랑 분리
 import os
 from datetime import datetime
 from pytz import timezone,utc
 from mailer import Mailer
+from memcached import Memcached
+import json
 
 os.system('curl -s "https://www.nike.com/kr/launch/?type=upcoming" | grep 응모 > /tmp/nike_temp.log')
 
 f = open('/tmp/nike_temp.log', 'r')
 mailer = Mailer()
+memcached = Memcached()
+schedule_key = 'nike_schedule'
 
 # TODO : 나이키 드로우 특성상 10시에 드로우 하므로 우선순위가 떨어져 시간에 대한 검증은 하지 않는다 추후 리팩토링 때.. 
 now = datetime.now()
@@ -18,6 +23,12 @@ month = now.month
 day = now.day
 
 send_condition_satisfied = False
+need_schedule_save = False
+# TODO : 저장소에 들어 있는 예정 정보를 불러와서 메일을 보낼지 말지 판단한다
+
+saved_schedule_info = memcached.get(schedule_key)
+if saved_schedule_info is None : saved_schedule_info = {}
+else : saved_schedule_info = json.loads(saved_schedule_info)
 
 while True:
     line = f.readline()
@@ -36,9 +47,10 @@ while True:
         for di in draw_info:
             if "/" in di:
                 date_info = di.split("/")
-                if(int(date_info[0]) == month and int(date_info[1]) == day):
-                    # TODO : db에 저장할까?
-                    pass
+                if date_info[0] not in saved_schedule_info : saved_schedule_info[date_info[0]] = {}
+                saved_schedule_info[date_info[0]][date_info[1]] = True
+                need_schedule_save = True
+
 f.close()
 
 if send_condition_satisfied == True:
@@ -49,3 +61,5 @@ if send_condition_satisfied == True:
     mailer.send_message(receivers)
     print('send message done')
 
+if need_schedule_save == True:
+    memcached.set(schedule_key, json.dumps(saved_schedule_info))
